479adcbbcac7be5da76c6dbf10158e1ab62bceeb
1lucaciu <lucaciu_semida@yahoo.com>
2018-07-23 10:40:19 +0300

    first

---
+++ AccesaEmployee/Employee.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace AccesaEmployee
+{
+	public class Employee
+	{
+        private  string _name;
+        public  EmployeePosition _position;
+        private  float _capacity;//max number of hours per day
+        private  List<string> _hobbies = new List<string>();
+        public const string XmlName = "employee";
+        public const string XmlHobbies = "hobby";
+        public Employee() { }
+        public Employee(XmlReader r) { ReadXml(r); }
+
+        public virtual void ReadXml(XmlReader r)
+        {
+            r.ReadStartElement();
+            _name = r.ReadElementContentAsString("name", "");
+            //_position = (EmployeePosition)Enum.Parse(typeof(EmployeePosition), r.ReadElementContentAsString("position", ""));
+            _capacity = r.ReadElementContentAsFloat("capacity", "");
+            r.ReadStartElement();
+
+            if (r.Name == "Hobbies")
+                while (r.NodeType == XmlNodeType.Element)
+                {
+                    if (r.Name == "Hobbies")
+                        Hobbies.Add(r.ReadElementContentAsString("hobby", ""));
+                }
+            r.ReadEndElement();
+            //_hobbies = r.ReadElementContentAsString("hobbies", "");
+            r.ReadEndElement();
+        }
+        public virtual void WriteXml(XmlWriter w)
+        {
+            w.WriteElementString("name", _name);
+            w.WriteElementString("capacity", _capacity.ToString());
+            //w.WriteElementString($"{EmployeePosition.QA}",_position );
+            foreach (string hobby in _hobbies)
+            {
+                w.WriteStartElement(Employee.XmlHobbies);
+                w.WriteElementString("hobby", hobby);
+                w.WriteEndElement();
+            }
+        }
+        public string Name { get { return _name; } set { _name = value; } }
+		public EmployeePosition Position { get { return _position; } set { _position = value; } }
+        public float Capacity { get { return _capacity; } set { _capacity = value; } }
+        public List<string> Hobbies => _hobbies;
+
+        protected Employee(string name, EmployeePosition position, float capacity)
+        {
+            _name = name;
+            _position = position;
+            _capacity = capacity;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            var sb = new StringBuilder();
+            _hobbies.ForEach(x => sb.Append(x + " "));
+            Console.WriteLine($"{_name} ocupa pozitia {_position} si e angajat cu {_capacity} ore pe zi. Lui ii place {sb.ToString()}");
+        }
+    }
+}
---
+++ AccesaEmployee/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace AccesaEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OfficeManagement officeManagement = new OfficeManagement();
+            // using (XmlReader reader = XmlReader.Create("Office.xml")) ;
+
+            //POPULEAZA LISTA DE ANGAJATI SI PROIECTE SI APOI CHEAMA WRITEXML
+
+
+            
+            //var officeManagement = new OfficeManagement();
+            /*officeManagement.DisplayAllProjects();
+
+            officeManagement.DeleteEmployee(dev);
+            officeManagement.DisplayAllEmployees();
+            officeManagement.DisplayAllProjects();*/
+
+            PopulateEmployeeList(officeManagement);
+            officeManagement.DisplayAllEmployees();
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            using (XmlWriter writer = XmlWriter.Create(@"c:\temp\temp.xml", settings))
+            {
+                officeManagement.WriteXml(writer);
+            }
+
+            Console.ReadLine();
+
+        }
+        //private static void PopulateEmployeeList(OfficeManagement office)
+        //{
+
+        //    Employee emp = new Employee();
+        //    emp.Name = "Ana";
+        //    emp.Position = EmployeePosition.Intern;
+        //    emp.Capacity = 8;
+
+
+
+        //}
+        private static void PopulateEmployeeList(OfficeManagement officeManagement)
+        {
+            var allInformation = File.ReadAllText(@"C:\Users\semida.lucaciu\Downloads\officeDB.txt");
+            char[] arr = new char[] { '\r', '\n' };
+            //var result = (from line in allInformation
+            //              let trimmedLine = line.TrimStart(arr)
+            //              select line).ToList();
+            //GetEmployeeFromText(result, officeManagement);
+            var employees = allInformation.Split(new string[] { nameof(Employee), "{", "}" }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var record in employees)
+            {
+                var trimmedRecord = record.TrimStart(new char[] { '\r', '\n' });
+                if (trimmedRecord.StartsWith(nameof(Project)) || trimmedRecord.Equals("\r\n")) continue;
+                GetEmployeeFromText(trimmedRecord, officeManagement);
+            }
+            //var query =
+            //    from n in employees
+            //    where n.Contains("Name:")
+            //    select n.ToUpper();
+        }
+
+        private static Employee GetEmployeeFromText(string info, OfficeManagement officeMangement)
+        {
+            if (string.IsNullOrEmpty(info)) return null;
+            var lines = info
+                    .Replace("\t", string.Empty)
+                    .Trim()
+                    .Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            if (!lines.Any()) return null;
+
+            var name = GetPropertyValue(nameof(Employee.Name), lines);
+            var position = GetPropertyValue(nameof(Employee.Position), lines);
+            var capacity = GetPropertyValue(nameof(Employee.Capacity), lines);
+            var hobbies = GetPropertyValue(nameof(Employee.Hobbies), lines).Split(',');
+
+            var positionType = EmployeePosition.Intern;
+            if (!Enum.TryParse(position, out positionType))
+                Console.WriteLine($"Pentru {name} nu s-a putut stabili position type");
+
+            return officeMangement.AddEmployee(name, positionType, Convert.ToSingle(capacity, CultureInfo.InvariantCulture), hobbies.ToList());
+        }
+
+        private static string GetPropertyValue(string propertyName, string[] values)
+        {
+            if (!values.Any()) return string.Empty;
+            var adjustedProperty = propertyName + ":";
+            return values.SingleOrDefault(x => x.StartsWith(adjustedProperty))
+                .TrimStart(adjustedProperty.ToCharArray())
+                .TrimEnd("\r\n".ToCharArray());
+        }
+    }
+}
